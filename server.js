a54261afea4bd4f8dcdea25fb06728ea2deb62c6
ines <1160766@isep.ipp.pt>
2018-09-10 22:30:07 +0100

    adds  database connectione

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt = require('bcrypt-nodejs');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-slippery-74920',
-    user : 'postgres',
-    password : 'password',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+  	ssl: true
   }
 });
 
