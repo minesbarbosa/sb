0604f753f1a4a179c24808b0e4d07fe296dd1f65
ines <1160766@isep.ipp.pt>
2018-09-10 19:11:12 +0100

    initiating project

---
+++ smart-brain-api/server.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : 'password',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then(data =>{
+	console.log(data);
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req,res) => {
+	res.send('this is working');
+})
+
+app.post('/signin', (req,res) => {
+	db.select('email','hash').from('login')
+	.where('email','=',req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if(isValid) {
+		return db.select('*').from('users')
+			.where('email','=', req.body.email)
+			.then(user => {
+				console.log(user);
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('unable to get user'))
+		} else {
+		res.status(400).json('wrong credentials')
+	}
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
+})
+
+//create new user
+app.post('/register', (req,res) => {
+	const {email, name, password} = req.body;
+	const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+			.returning('*')
+			.insert({
+				email: loginEmail[0],
+				name: name,
+				joined: new Date()
+			}).then(user => {
+				res.json(user[0]);		
+				})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})
+		.catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id',(req, res) => {
+	const {id} = req.params;
+	db.select('*').from('users').where({id: id})
+	.then(user => {
+		if(user.length) {
+			res.json(user[0])
+		} else {
+			res.status(400).json('Not found')
+		}
+	})
+	.catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+	const {id} = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(process.env.PORT || 3004,() => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT  --> user 
+*/
