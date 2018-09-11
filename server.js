2c41cb43d601100bad22056b46e0332a6904faca
ines <1160766@isep.ipp.pt>
2018-09-11 13:51:52 +0100

    update

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) => {res.send(database.users);})
-app.post('/signin', (req, res) => {signin.handleSignin(req,res,db,bcrypt)});
+app.post('/signin', (req, res) => {signin.handleSignin(req,res,database,bcrypt)});
 //create new user
-app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
-app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)});
-app.put('/image', (req,res) => {image.handleImage(req, res, db)});
+app.post('/register', (req,res) => {register.handleRegister(req,res,database,bcrypt)});
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,database)});
+app.put('/image', (req,res) => {image.handleImage(req, res, database)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)});
 app.listen(process.env.PORT || 3005, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
